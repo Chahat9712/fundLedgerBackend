5344d59a46dc2c240170b7ec126ff99e49ec3d4d
Chahat9712 <work.gup97@gmail.com>
2025-07-24 21:12:21 +0530

    change

--- src/main/java/com/hack/hack25/controller/FundController.java
+++ src/main/java/com/hack/hack25/controller/FundController.java
@@ -7,6 +7,7 @@ import com.hack.hack25.service.FundService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.util.Pair;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,47 +19,47 @@ public class FundController {
     @Autowired
     private FundService fundService;
 
-    @PostMapping
+    @PostMapping("/addUserToFund")
     public void addUserToExistingFund(String userId, String fundName)
     {
         fundService.addUserToFund(fundName, userId);
     }
 
-    @GetMapping
+    @GetMapping("/getAllFunds")
     public void getAllFunds(){
         fundService.getAllFunds();
     }
 
-    @PostMapping
+    @PostMapping("/user")
     public String registerUser(String name, double fundValue){
         return fundService.registerUser(name, fundValue);
     }
 
-    @GetMapping
+    @GetMapping("/getFundByName/{fundName}")
     public Fund getFundByName(String fundName)
     {
         return fundService.getFundByName(fundName);
     }
 
-    @GetMapping
-    public List<Fund> getAllFundsBasedOnUserId(String userId){
+    @GetMapping("/getUserFunds/{userId}")
+    public List<Fund> getAllFundsBasedOnUserId(@PathVariable String userId){
         return fundService.getAllFundsByUserId(userId);
     }
 
     /*List fund details --- participants, hight contributors, fund value, loan amount*/
 
-    @GetMapping
-    public Analytics getData(String fundName){
+    @GetMapping("/getAnalytics/{fundName}")
+    public Analytics getData(@PathVariable String fundName){
         return fundService.getAnalysis(fundName);
     }
 
-    @PostMapping
+    @PostMapping("/addFund")
     public String addNewFund(String fundName){
         return fundService.addNewFund(fundName);
     }
 
-    @GetMapping
-    public Pair<List<Fund>, List<Transaction>> getFundsAndParticipantsForUser(String userId){
+    @GetMapping("/getFundsTransactions/{userId}")
+    public Pair<List<Fund>, List<Transaction>> getFundsAndParticipantsForUser(@PathVariable String userId){
         return fundService.getFundsTransactionsForUser(userId);
     }
 }
--- src/main/java/com/hack/hack25/repository/FundRepository.java
+++ src/main/java/com/hack/hack25/repository/FundRepository.java
@@ -2,9 +2,11 @@ package com.hack.hack25.repository;
 
 import com.hack.hack25.model.Fund;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface FundRepository extends JpaRepository<Fund, String> {
 
     List<Fund> findByFundType(String fundType);
